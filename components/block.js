94d1e0de280fe8a1021c7ce2127ee6f9ac007a6c
Jon Penwood <jonp@polarity.io>
2023-10-18 11:30:38 -0500

    fix: Removed need to entity subtype

--- components/block.js
+++ components/block.js
@@ -1,6 +1,15 @@
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details'),
-  timezone: Ember.computed('Intl', function() {
+  timezone: Ember.computed('Intl', function () {
     return Intl.DateTimeFormat().resolvedOptions().timeZone;
-  })
+  }),
+  isKbEntity: false,
+  isCveEntity: false,
+  init() {
+    if (this.get('block.entity.type') === 'custom') {
+      if (this.get('block.entity.types').includes('custom.kb')) this.set('isKbEntity', true);
+      if (this.get('block.entity.types').includes('custom.cve')) this.set('isCveEntity', true);
+    }
+    this._super(...arguments);
+  }
 });
